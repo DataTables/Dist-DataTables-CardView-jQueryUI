e59c1e0ad54906b5365ba26388afc948e458117c
dtbuild <dtbuild@rigil>
2026-07-31 08:36:16 +0000

    220865f41de48460b4a9c7613b2956518344e915 Release 1.0.1
    Sync to source repo @220865f41de48460b4a9c7613b2956518344e915

--- js/cardView.jqueryui.min.mjs
+++ js/cardView.jqueryui.min.mjs
@@ -1,4 +1,4 @@
-/*! CardView jQuery UI styling 1.0.0 for DataTables
+/*! CardView jQuery UI styling 1.0.1 for DataTables
  * Copyright (c) SpryMedia Ltd - https://datatables.net/license/plus
  */
 import DataTable from"datatables.net-jqui";import CardView from"datatables.net-cardview";export default DataTable;
--- js/cardView.jqueryui.mjs
+++ js/cardView.jqueryui.mjs
@@ -1,4 +1,4 @@
-/*! CardView jQuery UI styling 1.0.0 for DataTables
+/*! CardView jQuery UI styling 1.0.1 for DataTables
  * Copyright (c) SpryMedia Ltd - https://datatables.net/license/plus
  */
 
